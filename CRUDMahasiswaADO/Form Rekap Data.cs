219d09fa47f1d7e56ffc8ed1636c26bb682d133f
Rayen1a <raihan01426.gmail.com>
2026-06-17 00:17:18 +0700

    membuat dan menambah DAL dbLogic kedalam class From_Rekap_Data

---
+++ CRUDMahasiswaADO/Form Rekap Data.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace CRUDMahasiswaADO
+{
+    public partial class Form_Rekap_Data: Form
+    {
+        static string connectionString = "Data Source=LAPTOP-BDD2S1H1\\RAIHAN_ALFAKHRI1;Initial Catalog=DBAkademikADO;User ID=sa;Password=SQLraihan1";
+        DAL dbLogic = new DAL();
+        
+        SqlConnection conn = new SqlConnection(connectionString);
+        SqlDataAdapter da;
+        DataTable dtMahasiswa;
+        DataTable dtProdi;
+        public Form_Rekap_Data()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
