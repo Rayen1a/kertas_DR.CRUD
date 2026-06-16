b86a2be26d378361f6f7d7384c304cc087cd9886
Rayen1a <raihan01426.gmail.com>
2026-06-17 00:25:36 +0700

    Membuat Form Baru Dashboard

---
+++ CRUDMahasiswaADO/FormDashboard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq.Expressions;
+using System.Web.ModelBinding;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace CRUDMahasiswaADO
+{
+    public partial class FormDashboard : Form
+    {
+        DAL dbLogic = new DAL();
+        bool isInitializing = true;
+        DataTable dt;
+        int button = 0;
+        public FormDashboard()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
