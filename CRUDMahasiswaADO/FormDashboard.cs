12c479c590c05e02e72db2509a6402f420f306b9
Rayen1a <raihan01426.gmail.com>
2026-06-17 00:27:17 +0700

    Membuat class FormDashboard_Load dan kodingannya

--- CRUDMahasiswaADO/FormDashboard.cs
+++ CRUDMahasiswaADO/FormDashboard.cs
@@ -20,6 +20,36 @@ namespace CRUDMahasiswaADO
             InitializeComponent();
         }
 
+        private void FormDashboard_Load(object sender, EventArgs e)
+        {
+            dtpTanggalMasuk.Format = DateTimePickerFormat.Custom;
+            dtpTanggalMasuk.CustomFormat = "yyyy";
+            dtpTanggalMasuk.ShowUpDown = true;
+            dtpTanggalMasuk.MinDate = new DateTime(2000, 1, 1);
+            dtpTanggalMasuk.MaxDate = DateTime.Now;
+
+            cmbTipe.DropDownStyle = ComboBoxStyle.DropDownList;
+            var items = new List<KeyValuePair<string, SeriesChartType>>
+            {
+                new KeyValuePair<string, SeriesChartType>("Column", SeriesChartType.Column),
+                new KeyValuePair<string, SeriesChartType>("Pie", SeriesChartType.Pie)
+            };
+
+            isInitializing = true;
+            cmbTipe.DataSource = items;
+            cmbTipe.DisplayMember = "Key";
+            cmbTipe.ValueMember = "Value";
+            cmbTipe.SelectedIndex = 0;
+
+            if (isInitializing)
+                return;
+            if (button == 1) {
+            }
+            else {
+                loadDataChart();
+            }
+        }
+
         
     }
 }
