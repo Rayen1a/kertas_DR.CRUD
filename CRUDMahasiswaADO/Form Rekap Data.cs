34e892785a71c4378b46ef0901aad25228243dca
Rayen1a <raihan01426.gmail.com>
2026-06-22 11:55:32 +0700

    memasukkan form rekap data uptade

--- CRUDMahasiswaADO/Form Rekap Data.cs
+++ CRUDMahasiswaADO/Form Rekap Data.cs
@@ -8,12 +8,11 @@ namespace CRUDMahasiswaADO
     public partial class Form_Rekap_Data: Form
     {
         static string connectionString = "Data Source=LAPTOP-BDD2S1H1\\RAIHAN_ALFAKHRI1;Initial Catalog=DBAkademikADO;User ID=sa;Password=SQLraihan1";
-        DAL dbLogic = new DAL();
-        
         SqlConnection conn = new SqlConnection(connectionString);
         SqlDataAdapter da;
         DataTable dtMahasiswa;
         DataTable dtProdi;
+        
         public Form_Rekap_Data()
         {
             InitializeComponent();
@@ -99,6 +98,11 @@ namespace CRUDMahasiswaADO
             Form_Report frm2 = new Form_Report(cmbProdi.SelectedValue.ToString(), dtpTanggalMasuk.Value);
             frm2.Show();
             this.Hide();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
